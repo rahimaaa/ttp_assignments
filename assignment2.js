a8fed65ff6e4340ae0117e55edc97417b9e061a0
rakhima khabibullaeva <rakhimakhabib@gmail.com>
2023-06-18 13:48:23 -0400

    added functions mySome(), myEvery(), myReduce(), myIncludes(), myIndexOf(), myPush(), myLastIndexOf(),

--- assignment2.js
+++ assignment2.js
@@ -1,116 +1,154 @@
-
-
+const arr1 = ["ado", "opo", "hapi", "abdu"];
+const arr2 = [1, 2, 3, 4, 5, 6, 2];
 
 // MYEACH
-const names = ['ado','opo','hapi', 'abdu'];
+
 //from my understanding its supposed to take in an array
 //and iterate through each element with a callback of my choosing
- function myEach (arr, cb) {
-    for(let i = 0; i< arr.length; i++){
-        cb(arr[i])
-    }
+function myEach(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i]);
+  }
 }
-//myEach(names)
+//myEach(arr1)
 
+//MYMAP
+//myMap function, takes in an array and outputs the arra with whatever
+//the callback function does, in this example it takes the arraya and the 2nd
+//function multiplies an element by 2, and the myMap function
+//iterates through the array and returns all the elements multiplied by two
 
-    //MYMAP
-    //myMap function, takes in an array and outputs the arra with whatever
-    //the callback function does, in this example it takes the arraya and the 2nd
-    //function multiplies an element by 2, and the myMap function
-    //iterates through the array and returns all the elements multiplied by two
-   const  arr2 = [1,2,3,4,5, 6]
-   function myMap (arr, cb){
-      let result = [];
-       for (let i = 0; i < arr.length; i++) {
-            result[i] = cb(arr[i]);
-        }
-     return result;
-   } 
+function myMap(arr, cb) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = cb(arr[i]);
+  }
+  return result;
+}
 //    const vari=(value)=>{ return value * 2}
 //    const mapp = myMap(arr2,vari )
 //    console.log(mapp)
 
-
-
 //MYFILTER
-//iterterates through the array and returns if the passed element 
-//onith position in the array holds true for the callback function 
+//iterterates through the array and returns if the passed element
+//onith position in the array holds true for the callback function
 //that we/user passed
-  function myFilter(arr, cb){
-    let  result =[];
-    for(let i = 0; i<arr.length; i++){ 
-      if(cb(arr[i])){
-          result[result.length] = arr[i];
-      
-        } 
+function myFilter(arr, cb) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i])) {
+      result[result.length] = arr[i];
     }
-    return result;
   }
+  return result;
+}
 
 //   const div3 =(value)=>{ return value % 3 == 0}
 //   console.log(myFilter(arr2, div3))
 
-
-
-
 //MYSOME
-//
 
-function mySome(){
-
-
-    return
+function mySome(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i])) return true;
+  }
+  return false;
 }
+
+//const some = (value)=>{return value % 7 == 0 }
+// console.log(mySome(arr2,some))
 
 //EVERY
 
-function myEvery(){
-     
+function myEvery(arr, cb) {
+  for (let i in arr) {
+    if (!cb(i)) return false;
+  }
+  return true;
 }
+
+//console.log(myEvery(arr2, some))
 
 //REDUCE
 
-function myReduce(){
-
+function myReduce(arr, cb, value) {
+  for (let i in arr) {
+    //had to use === for strict equality
+    if (value === undefined) {
+      value = i;
+    } else {
+      value = cb(arr[i], value);
+    }
+  }
+  return value;
 }
+//   const add =(val, i)=>{ return i= i + val}
+//   console.log(myReduce(arr2, add, 0))
 
 //INCLUDES
-
-function myIncludes(){
-
+function myIncludes(arr, element) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == element) {
+      return true;
+    }
+  }
+  return false;
 }
+
+//console.log(myIncludes(arr1,'seva'))
 
 //INDEX OF
-
-function myIndexOf(){
-
+function myIndexOf(arr, element) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == element) {
+      return i;
+    }
+  }
+  return "not found";
 }
-
+//console.log(myIndexOf(arr2, 7))
 
 //MYPUSH
+function myPush(arr, element) {
+  let length = arr.length;
 
-function myPush(){
-
-
+  if (typeof element == "object") {
+    for (let i of element) {
+      arr[length] = i;
+      length++;
+    }
+  } else {
+    arr[length] = element;
+    length++;
+  }
+  return arr.length;
 }
+// let arr3 = ['cows', 'dogs', 'cats']
+// console.log(myPush(arr3,'pigs'))
+// console.log(arr3)
 
 //LAST INDEX OF
- function myLastIndexOf(){
-
-
- }
-
-//OBJECT KEYS, 
-
-function grabKeys(){
-
+function myLastIndexOf(arr, element) {
+  let x = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == element) {
+      x = i;
+    }
+  }
+  return x;
 }
+//console.log(myLastIndexOf(arr2,2))
+
+//OBJECT KEYS,
+/*
+ Modify the Object class, appending a static method to the Object class.,
+  appending a static method to the Object class. Leverage the “for...in” 
+  loop.
+*/
+function grabKeys() {}
 
 //OBJECT VALUES
-function grabValues(){
-
-    
-}
+function grabValues() {}
 
 /* PROBLEM(1)
 1: Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
@@ -130,4 +168,3 @@ You must do this in-place without making a copy of the array.
 
 
 */
-        
