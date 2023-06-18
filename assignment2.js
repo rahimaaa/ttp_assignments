cdaf24f89513874e4c2503e4db593c3d12f4a6f0
rakhima khabibullaeva <rakhimakhabib@gmail.com>
2023-06-18 12:27:50 -0400

    added functions forEach(), myMap(), myFilter()

--- assignment2.js
+++ assignment2.js
@@ -1,34 +1,133 @@
-// myEach function
+
+
+
+// MYEACH
 const names = ['ado','opo','hapi', 'abdu'];
-
-const myEach = (arr , cb) => {
-    for( let i = 0; i < arr.length; i++){
-        const element = arr[i];
-        console.log(element);
-        cb(element)
-
+//from my understanding its supposed to take in an array
+//and iterate through each element with a callback of my choosing
+ function myEach (arr, cb) {
+    for(let i = 0; i< arr.length; i++){
+        cb(arr[i])
     }
 }
-myEach(names,(name) => {
+//myEach(names)
 
-    console.log(myEach);
-    })
 
-    //myMap function
-   const  arr2 = [1,2,3,4,5]
-
-    function myMap (arr2,cb){
-        var newArr = [];
-        for(let i =1; i<arr2.length; i++){
-            newArr.push(cb(arr2[i]*2))
-
+    //MYMAP
+    //myMap function, takes in an array and outputs the arra with whatever
+    //the callback function does, in this example it takes the arraya and the 2nd
+    //function multiplies an element by 2, and the myMap function
+    //iterates through the array and returns all the elements multiplied by two
+   const  arr2 = [1,2,3,4,5, 6]
+   function myMap (arr, cb){
+      let result = [];
+       for (let i = 0; i < arr.length; i++) {
+            result[i] = cb(arr[i]);
         }
-        console.log(newArray);
-        return newArr
-        
+     return result;
+   } 
+//    const vari=(value)=>{ return value * 2}
+//    const mapp = myMap(arr2,vari )
+//    console.log(mapp)
+
+
+
+//MYFILTER
+//iterterates through the array and returns if the passed element 
+//onith position in the array holds true for the callback function 
+//that we/user passed
+  function myFilter(arr, cb){
+    let  result =[];
+    for(let i = 0; i<arr.length; i++){ 
+      if(cb(arr[i])){
+          result[result.length] = arr[i];
+      
+        } 
     }
+    return result;
+  }
+
+//   const div3 =(value)=>{ return value % 3 == 0}
+//   console.log(myFilter(arr2, div3))
+
+
+
+
+//MYSOME
+//
+
+function mySome(){
+
+
+    return
+}
+
+//EVERY
+
+function myEvery(){
+     
+}
+
+//REDUCE
+
+function myReduce(){
+
+}
+
+//INCLUDES
+
+function myIncludes(){
+
+}
+
+//INDEX OF
+
+function myIndexOf(){
+
+}
+
+
+//MYPUSH
+
+function myPush(){
+
+
+}
+
+//LAST INDEX OF
+ function myLastIndexOf(){
+
+
+ }
+
+//OBJECT KEYS, 
+
+function grabKeys(){
+
+}
+
+//OBJECT VALUES
+function grabValues(){
+
     
+}
 
-    console.log(myMap(arr2));
+/* PROBLEM(1)
+1: Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+function moveZeros (nums) {
+
+}
 
 
+Example:
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+
+Note:
+You must do this in-place without making a copy of the array.
+
+
+*/
+        
--- assignment3/script.js
+++ assignment3/script.js
@@ -1,1 +1,35 @@
-//create script file
+
+//Selecting Section
+
+// 1 - select the section with an id of container without the querySelector
+
+// 2 - select the section with an id of container with the querySelector
+
+// 3 - List elements with class second
+
+var elements = document. getElementsByClassName(second);
+
+// 4 - List elements with class Third inside ol
+
+
+// 5 - Add Text Hello
+
+
+// 6 - Add the class main to footer
+
+
+// 7 - remove class main to footer
+
+
+// 8 - create a new li element
+
+
+// 9 - li text = "four"
+li.textContent = "four";
+
+// 10 append li to ul
+
+
+// 12 - Loop over the lis inside the ole tag and give them a brackground color of green
+
+// 13 - remove div with class footer
